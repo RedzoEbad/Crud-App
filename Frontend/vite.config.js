019c94220f444e8c7bd14f7e58b28b3c2fc17e1e
ebadkhan <ebadkhan6292002@gmail.com>
2025-10-30 19:02:08 +0500

    first commit new

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// ✅ Configure Vite Dev Server proxy
 export default defineConfig({
   plugins: [react()],
+ server: {
+  proxy: {
+    '/api': {
+      target: 'http://backend:8000',
+      changeOrigin: true,
+    },
+  },
+},
 })
